d0b83bb02a69c96327ecc4aa4680f83995157065
Junseo <oponize@naver.com>
2022-10-13 23:55:35 +0900

    feat: #1 Typography Component 추가

---
+++ src/components/common/typography.tsx
@@ -0,0 +1,67 @@
+import { forwardRef } from "react";
+import theme from "constants/theme";
+import type { AllHTMLAttributes, CSSProperties, ReactNode, Ref } from "react";
+
+type FontSize = keyof typeof theme.fontSize;
+
+type FontWeight = keyof typeof theme.fontWeight;
+
+type LineHeight = "120%" | "150%";
+
+export interface BaseProps {
+  children?: ReactNode;
+  className?: string;
+  fontSize?: FontSize;
+  fontWeight?: FontWeight;
+  lineHeight?: LineHeight;
+  color?: string;
+  display?: CSSProperties["display"];
+  textAlign?: CSSProperties["textAlign"];
+  /**
+   * @description 줄바꿈을 허용할지 말지를 결정합니다. white-space: pre-wrap
+   * @default false
+   */
+  space?: boolean;
+}
+
+type TypographyProps<Element extends keyof JSX.IntrinsicElements = "span"> =
+  BaseProps & {
+    as?: Element;
+  } & Omit<AllHTMLAttributes<Element>, "as">;
+
+const Typography = <Element extends keyof JSX.IntrinsicElements = "span">(
+  props: TypographyProps<Element>,
+  ref: Ref<HTMLElement>
+) => {
+  const {
+    as: Component = "span",
+    fontSize,
+    fontWeight,
+    lineHeight,
+    display = "inline-block",
+    textAlign,
+    children,
+    color,
+    space = false,
+    role,
+  } = props as TypographyProps;
+  return (
+    <Component
+      ref={ref}
+      role={role ?? (Component === "span" ? "text" : undefined)}
+      style={{
+        color,
+        fontSize: fontSize && theme.fontSize[fontSize],
+        fontWeight,
+        lineHeight,
+        display,
+        textAlign,
+        whiteSpace: space ? "pre-wrap" : "normal",
+      }}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export default forwardRef(Typography);
