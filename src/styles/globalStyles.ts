b7951d3258b7ca2d4e4417238ad76945370f4bcf
seongjin <seongjin2427@gmail.com>
2022-10-13 21:41:22 +0900

    feat: font 'Pretendard' apply in global

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,8 +1,21 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
   ${reset}
+
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/fonts/Pretendard-Bold.otf') format('opentype'), url('/font/Pretendard-Bold.ttf') format('ttf');
+    font-weight: 700;
+    font-display: swap;
+  }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('/fonts/Pretendard-Regular.otf') format('opentype'), url('/font/Pretendard-Regular.ttf') format('ttf');
+    font-weight: 400;
+    font-display: swap;
+  }
 
   html, body ,#__next  {
     width: 100%;
