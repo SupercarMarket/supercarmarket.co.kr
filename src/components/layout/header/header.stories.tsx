85719c241a21db7153b342b53f2d7dd54f61a5ca
박준서 <oponize@naver.com>
2022-10-16 16:55:57 +0900

    feat: init header story

---
+++ src/components/layout/header/header.stories.tsx
@@ -0,0 +1,22 @@
+import { expect } from '@storybook/jest';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+
+import Header from './header';
+
+export default {
+  title: 'Header',
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+const Template: ComponentStory<typeof Header> = () => <Header />;
+
+export const Default = Template.bind({});
+
+Default.argTypes = {};
+
+Default.args = {};
+
+Default.play = async ({ args, canvasElement }) => {
+  const canvas = within(canvasElement);
+};
