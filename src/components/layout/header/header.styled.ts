144411fbaf5c9f555584a360130f143419763bd5
박준서 <oponize@naver.com>
2022-10-16 16:56:30 +0900

    feat: init header component

--- src/components/layout/header/header.styled.ts
+++ src/components/layout/header/header.styled.ts
@@ -2,8 +2,16 @@ import styled from 'styled-components';
 
 const Container = styled.header`
   width: 100%;
+  height: 96px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
-export { Container };
+const Buttons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 32px;
+`;
+
+export { Buttons, Container };
--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,9 +1,37 @@
+import Button from 'components/common/button';
+import Link from 'components/common/link';
+import Logo from 'components/common/logo';
+import Typography from 'components/common/typography';
+
 import * as Styled from './header.styled';
 
 const Header = () => {
   return (
     <Styled.Container>
-      <h1></h1>
+      <Logo />
+      <Styled.Buttons>
+        <Link href="">
+          <Typography
+            color="black"
+            fontSize="body-16"
+            fontWeight="regular"
+            lineHeight="150%"
+          >
+            로그인
+          </Typography>
+        </Link>
+        <Link href="">
+          <Typography
+            color="black"
+            fontSize="body-16"
+            fontWeight="regular"
+            lineHeight="150%"
+          >
+            회원가입
+          </Typography>
+        </Link>
+        <Button variant="Line">문의하기</Button>
+      </Styled.Buttons>
     </Styled.Container>
   );
 };
