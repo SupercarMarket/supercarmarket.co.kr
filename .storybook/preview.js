ccd42516e5cbb08a1f098550d38120d39da93615
박준서 <oponize@naver.com>
2022-10-18 13:43:46 +0900

    feat: add storybook addons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,20 @@
+import { addDecorator } from '@storybook/react';
+import { withThemesProvider } from 'storybook-addon-styled-component-theme';
+import { ThemeProvider } from 'styled-components';
+import theme from '../src/constants/theme';
+import GlobalStyle from '../src/styles/globalStyles';
+
+addDecorator(withThemesProvider([theme]), ThemeProvider);
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
