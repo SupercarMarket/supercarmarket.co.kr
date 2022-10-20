2f3a165bf98e41319cbbb6964ca7bfc08b23aa2d
박준서 <oponize@naver.com>
2022-10-20 19:58:16 +0900

    feat: add Magazine Banner Component and Story

--- src/components/magazine/magazineBanner/magazineBanner.styled.ts
+++ src/components/magazine/magazineBanner/magazineBanner.styled.ts
@@ -1,11 +1,61 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const Container = styled.section``;
-
-const ImageWrapper = styled.div``;
-
-const ContentsWrapper = styled.div`
-  width: 480px;
+const contentsEllipsis = (line: number) => css`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box !important;
+  -webkit-line-clamp: ${line};
+  -webkit-box-orient: vertical;
+  word-wrap: break-word;
+  line-height: 1.2em;
 `;
 
-export { Container, ContentsWrapper, ImageWrapper };
+const Container = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 1200px;
+  height: 394px;
+  gap: 10px;
+  &.mb-reverse {
+    flex-direction: row-reverse;
+  }
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  width: 590px;
+  height: 394px;
+  gap: 16px;
+`;
+
+const ContentsWrapper = styled.div`
+  width: 590px;
+  height: 394px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+  fill: #fff;
+  .mb-contents-heading {
+    width: 480px;
+    ${contentsEllipsis(2)}
+  }
+  .mb-contents-body {
+    width: 480px;
+    ${contentsEllipsis(3)}
+  }
+  .mb-contents-body.mb-button {
+    ${contentsEllipsis(2)}
+  }
+  .mb-button {
+  }
+`;
+
+const ButtonWrapper = styled.div`
+  width: 480px;
+  display: flex;
+`;
+
+export { ButtonWrapper, Container, ContentsWrapper, ImageWrapper };
--- src/components/magazine/magazineBanner/magazineBanner.tsx
+++ src/components/magazine/magazineBanner/magazineBanner.tsx
@@ -1,20 +1,39 @@
+import clsx from 'clsx';
+import Button from 'components/common/button';
 import Typography from 'components/common/typography';
+import Image from 'next/image';
 
+import Arrow from '../../../assets/svg/arrow-right.svg';
 import * as Styled from './magazineBanner.styled';
 
 interface MagazineBannerProps {
   reverse?: boolean;
+  className?: string;
+  button?: boolean;
 }
 
-const MagazineBanner = ({}: MagazineBannerProps) => {
+const MagazineBanner = ({
+  reverse,
+  button,
+  className,
+}: MagazineBannerProps) => {
   return (
-    <Styled.Container>
+    <Styled.Container
+      className={clsx(
+        'mb',
+        {
+          [`mb-reverse`]: reverse,
+        },
+        className
+      )}
+    >
       <Styled.ContentsWrapper>
         <Typography
           as="span"
           color="system-1"
           style={{
-            marginBottom: '16px',
+            width: '480px',
+            textAlign: 'start',
           }}
         >
           NEW
@@ -25,9 +44,7 @@ const MagazineBanner = ({}: MagazineBannerProps) => {
           fontWeight="bold"
           color="black"
           lineHeight="150%"
-          style={{
-            marginBottom: '12px',
-          }}
+          className={clsx('mb-contents-heading')}
         >
           람보르기니 우루스 퍼포만테 더 슈퍼 SUV하다.
         </Typography>
@@ -37,13 +54,40 @@ const MagazineBanner = ({}: MagazineBannerProps) => {
           color="black"
           fontWeight="regular"
           lineHeight="150%"
+          className={clsx('mb-contents-body', {
+            [`mb-button`]: button,
+          })}
         >
           슈퍼하고도 슈퍼SUV모델 등장 람보르기니 우루스 퍼포만테 과연 우라칸의
           이어 갈 수 있을까? 오늘 소개해드릴 차량은 첫 등장부터 모두의 관심을
           받았고, 지금도 관심을 받고 있는 슈퍼 SUV입니다. 그것도 엄청난 ...
         </Typography>
+        {button && (
+          <Styled.ButtonWrapper>
+            <Button
+              variant="Black"
+              border="rounded"
+              type="button"
+              suffix={<Arrow />}
+            >
+              보러가기
+            </Button>
+          </Styled.ButtonWrapper>
+        )}
       </Styled.ContentsWrapper>
-      <Styled.ImageWrapper></Styled.ImageWrapper>
+      <Styled.ImageWrapper>
+        <Image
+          src="https://user-images.githubusercontent.com/66871265/196571825-f136a62d-15f3-4d21-a709-8ea0fd77f98a.png"
+          alt="thumbnail"
+          layout="fixed"
+          width={590}
+          height={394}
+          style={{
+            borderRadius: '4px',
+          }}
+          priority
+        />
+      </Styled.ImageWrapper>
     </Styled.Container>
   );
 };
