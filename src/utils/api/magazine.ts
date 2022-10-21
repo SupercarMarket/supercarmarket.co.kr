a1e71e0133f6ff0b68b4b7de9d32f62c05182aa0
박준서 <oponize@naver.com>
2022-10-21 16:32:23 +0900

    refactor: magazine-banner api logic

---
+++ src/utils/api/magazine.ts
@@ -0,0 +1,36 @@
+import type { NextApiHandler } from 'next/types';
+import { getPlaiceholder } from 'plaiceholder';
+import { MagazineDto, MagazineResponse } from 'types/magazine';
+import { getErrorMessage } from 'utils/misc';
+
+const magazineApi: NextApiHandler = async (_, res) => {
+  try {
+    const response = await fetch(
+      'http://localhost:3000/server/api/v1/magazine',
+      { method: 'GET' }
+    );
+
+    if (!response.ok) throw new Error('invalid api');
+
+    const magazine: MagazineResponse<MagazineDto> = await response.json();
+
+    const magazineWithBluredImage = await Promise.all(
+      magazine.data.map(async (m) => {
+        const { base64 } = await getPlaiceholder(m.imgSrc);
+        return {
+          ...m,
+          base64,
+        };
+      })
+    ).then((v) => v);
+
+    return res.status(200).json({
+      ...magazine,
+      data: magazineWithBluredImage,
+    });
+  } catch (e) {
+    throw new Error(getErrorMessage(e));
+  }
+};
+
+export { magazineApi };
