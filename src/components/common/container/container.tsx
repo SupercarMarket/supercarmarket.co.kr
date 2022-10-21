6791143999e1c212688314de8704e67e1bcf9c49
박준서 <oponize@naver.com>
2022-10-21 18:33:30 +0900

    feat: Add Conatiner Component

---
+++ src/components/common/container/container.tsx
@@ -0,0 +1,57 @@
+import clsx from 'clsx';
+import type { CSSProperties, ReactNode } from 'react';
+
+interface ContainerProps {
+  className?: string;
+  element?: HTMLElement;
+  children?: ReactNode;
+  width?: CSSProperties['width'];
+  height?: CSSProperties['height'];
+  padding?: CSSProperties['padding'];
+  margin?: CSSProperties['margin'];
+  position?: CSSProperties['position'];
+  display?: CSSProperties['display'];
+  flexDirection?: CSSProperties['flexDirection'];
+  justifyContent?: CSSProperties['justifyContent'];
+  alignItems?: CSSProperties['alignItems'];
+}
+
+const Container = (props: ContainerProps) => {
+  const {
+    element = 'div',
+    width = '100%',
+    height,
+    padding = '0',
+    margin = '0',
+    position,
+    display,
+    justifyContent,
+    flexDirection,
+    alignItems,
+    className,
+    children,
+  } = props;
+  const Component: React.ComponentType<React.HTMLAttributes<HTMLDivElement>> =
+    element as any;
+  return (
+    <Component
+      className={clsx('container', className)}
+      style={{
+        width,
+        height,
+        padding,
+        margin,
+        position,
+        display,
+        flexDirection,
+        justifyContent,
+        alignItems,
+        maxWidth: '100%',
+      }}
+    >
+      {children}
+    </Component>
+  );
+};
+
+export default Container;
