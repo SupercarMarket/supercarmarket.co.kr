d005103680c12fd6bcd903de402750e0a310d46b
Junseo <oponize@naver.com>
2022-11-01 15:57:13 +0900

    feat: init home magazine list

---
+++ src/components/home/magazine/magazine.style.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin: 40px 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 20px;
+`;
+
+export { Wrapper };
