ac3a1abe3b1e6449ca10757c57a5cc5c4ea28ccc
seongjin <seongjin2427@gmail.com>
2022-11-04 23:54:41 +0900

    fix: fix market-car-category story

--- src/components/market/market-car-category/market-car-category.stories.tsx
+++ src/components/market/market-car-category/market-car-category.stories.tsx
@@ -14,8 +14,8 @@ const Template: ComponentStory<typeof MarketCarKind> = (args) => (
 export const Default = Template.bind({});
 
 Default.argTypes = {
-  kind: {
-    name: 'Kind',
+  category: {
+    name: 'Category',
     description: '현재 필터링하는 차 종류를 나타내는 문자열',
     options: CATEGORY_VALUES,
     control: {
@@ -25,5 +25,5 @@ Default.argTypes = {
 };
 
 Default.args = {
-  kind: 'all',
+  category: 'all',
 };
