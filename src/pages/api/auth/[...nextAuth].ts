570bbd08aa19101d5489366ef4d8046cc02a5e6d
박준서 <oponize@naver.com>
2022-12-05 21:01:25 +0900

    feat: next-auth 세팅

--- src/pages/api/auth/[...nextAuth].ts
+++ src/pages/api/auth/[...nextAuth].ts
@@ -1,44 +1,68 @@
-import type { NextAuthOptions } from 'next-auth';
+import type { NextApiHandler } from 'next';
+import type {
+  Account,
+  CallbacksOptions,
+  NextAuthOptions,
+  Profile,
+} from 'next-auth';
 import NextAuth from 'next-auth';
+import type { Provider } from 'next-auth/providers';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
 import KakaoProvider from 'next-auth/providers/kakao';
 import { env } from 'utils/misc';
 
-const nextAuthOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        username: { label: 'Username', type: 'text' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize() {
-        const user = { id: '1', name: 'junseo', email: 'junseo@naver.com' };
+const providers: Provider[] = [
+  CredentialsProvider({
+    name: 'Credentials',
+    credentials: {
+      username: { label: 'Username', type: 'text' },
+      password: { label: 'Password', type: 'password' },
+    },
+    async authorize() {
+      const user = { id: '1', name: 'junseo', email: 'junseo@naver.com' };
 
-        if (user) {
-          return user;
-        } else {
-          return null;
-        }
+      if (user) {
+        return user;
+      } else {
+        return null;
+      }
+    },
+  }),
+  GoogleProvider({
+    clientId: env('GOOGLE_ID'),
+    clientSecret: env('GOOGLE_SECRET'),
+    authorization: {
+      params: {
+        prompt: 'consent',
+        access_type: 'offline',
+        response_type: 'code',
       },
-    }),
-    GoogleProvider({
-      clientId: env('GOOGLE_ID'),
-      clientSecret: env('GOOGLE_SECRET'),
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
-    }),
-    KakaoProvider({
-      clientId: env('KAKAO_CLIENT_ID'),
-      clientSecret: env('KAKAO_CLIENT_SECRET'),
-    }),
-  ],
+    },
+  }),
+  KakaoProvider({
+    clientId: env('KAKAO_CLIENT_ID'),
+    clientSecret: env('KAKAO_CLIENT_SECRET'),
+  }),
+];
+
+const callbacks: Partial<CallbacksOptions<Profile, Account>> | undefined = {
+  jwt({ token }) {
+    return token;
+  },
+  session({ session, token, user }) {
+    return session;
+  },
 };
 
-export default NextAuth(nextAuthOptions);
+const nextAuthOptions: NextAuthOptions = {
+  providers,
+  callbacks,
+  secret: process.env.NEXT_PUBLIC_AUTH_SECRET,
+};
+
+const NextAuthHandler: NextApiHandler = (req, res) =>
+  NextAuth(req, res, nextAuthOptions);
+
+export { nextAuthOptions };
+export default NextAuthHandler;
