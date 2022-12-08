7bb76f9463e1c417476eec40240d2a79f99f2fcd
박준서 <oponize@naver.com>
2022-12-08 14:05:31 +0900

    chore: auth 타입 스펙 변경

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,8 @@
 namespace NodeJS {
   interface ProcessEnv extends NodeJS.ProcessEnv {
+    NEXT_PUBLIC_URL: string;
+    NEXT_PUBLIC_SERVER_URL: string;
+    // auth
     NEXT_PUBLIC_AUTH_SECRET: string;
     NEXT_PUBLIC_GOOGLE_ID: string;
     NEXT_PUBLIC_GOOGLE_SECRET: string;
