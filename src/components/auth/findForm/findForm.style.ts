11825d44f0985bcd8a9007615648b130c847987c
박준서 <oponize@naver.com>
2022-12-10 17:41:36 +0900

    feat: Auth Find Page 마크업

---
+++ src/components/auth/findForm/findForm.style.ts
@@ -0,0 +1,13 @@
+'use client';
+
+import styled from 'styled-components';
+
+const Form = styled.form`
+  width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 26px;
+`;
+
+export { Form };
