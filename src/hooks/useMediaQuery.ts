fdd1acf0f8b900b2c8b66c521c9e4797d2359436
Junseo <oponize@naver.com>
2022-12-14 22:21:08 +0900

    feat: 미디어쿼리 훅 추가

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+export default function useMediaQuery(query: string): boolean {
+  const getMatches = (query: string): boolean => {
+    if (typeof window !== 'undefined') {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+
+  const [matches, setMatches] = React.useState<boolean>(getMatches(query));
+
+  function handleChange() {
+    setMatches(getMatches(query));
+  }
+
+  React.useEffect(() => {
+    const matchMedia = window.matchMedia(query);
+
+    handleChange();
+
+    if (matchMedia.addListener) {
+      matchMedia.addListener(handleChange);
+    } else {
+      matchMedia.addEventListener('change', handleChange);
+    }
+
+    return () => {
+      if (matchMedia.removeListener) {
+        matchMedia.removeListener(handleChange);
+      } else {
+        matchMedia.removeEventListener('change', handleChange);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  return matches;
+}
