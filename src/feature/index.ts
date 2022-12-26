52493a9eed18c54fef3cdc2f1b8301b2755888e1
Junseo <oponize@naver.com>
2022-12-26 18:22:43 +0900

    fix: merge conflict

--- src/feature/index.ts
+++ src/feature/index.ts
@@ -1,29 +1,17 @@
 type Action<ActionType extends string> =
   | {
       type: ActionType;
-<<<<<<< HEAD
       error?: null;
-=======
-      error?: boolean;
->>>>>>> 45c355dfdce16a4132d1d52bd9d7eabb4caf0864
       data?: unknown;
     }
   | {
       type: `${ActionType}_SUCCESS`;
-<<<<<<< HEAD
       error?: null;
-=======
-      error?: boolean;
->>>>>>> 45c355dfdce16a4132d1d52bd9d7eabb4caf0864
       data?: unknown;
     }
   | {
       type: `${ActionType}_ERROR`;
-<<<<<<< HEAD
       error?: Error;
-=======
-      error?: boolean;
->>>>>>> 45c355dfdce16a4132d1d52bd9d7eabb4caf0864
       data?: unknown;
     };
 type Dispatch<ActionType extends string> = (action: Action<ActionType>) => void;
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,7 +3,6 @@ import { JWT } from 'next-auth/jwt';
 
 declare module 'next-auth' {
   interface Session {
-<<<<<<< HEAD
     sub: string;
     accessToken: string;
     refreshToken: string;
@@ -18,24 +17,15 @@ declare module 'next-auth' {
     expire: number;
     provider: 'local' | 'kakao' | 'google';
     verified: boolean;
-=======
-    user: {
-      email: string;
-    };
->>>>>>> 45c355dfdce16a4132d1d52bd9d7eabb4caf0864
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
-<<<<<<< HEAD
     accessToken: string;
     refreshToken: string;
     expire: number;
     sub: string;
     provider: 'local' | 'kakao' | 'google';
-=======
-    accessToken?: string;
->>>>>>> 45c355dfdce16a4132d1d52bd9d7eabb4caf0864
   }
 }
