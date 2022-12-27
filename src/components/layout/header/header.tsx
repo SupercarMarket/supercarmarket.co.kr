175a2b198e4f5e070d8e505356ef77bc19b281be
박준서 <oponize@naver.com>
2022-12-27 16:02:38 +0900

    feat: 레이아웃 Link 적용

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -3,10 +3,13 @@ import Logo from 'components/common/logo';
 import Searchbar from 'components/common/searchbar';
 import Typography from 'components/common/typography';
 import Link from 'next/link';
+import { signOut, useSession } from 'next-auth/react';
 
 import * as Styled from './header.styled';
 
 const Header = () => {
+  const { status, data: session } = useSession();
+
   return (
     <Styled.Container>
       <Logo />
@@ -17,26 +20,61 @@ const Header = () => {
         width="507px"
       />
       <Styled.Buttons>
-        <Link href="">
-          <Typography
-            color="black"
-            fontSize="body-16"
-            fontWeight="regular"
-            lineHeight="150%"
-          >
-            로그인
-          </Typography>
-        </Link>
-        <Link href="">
-          <Typography
-            color="black"
-            fontSize="body-16"
-            fontWeight="regular"
-            lineHeight="150%"
-          >
-            회원가입
-          </Typography>
-        </Link>
+        {status === 'loading' ? (
+          'loading...'
+        ) : status === 'authenticated' ? (
+          <>
+            <Button
+              variant="Init"
+              style={{
+                cursor: 'pointer',
+              }}
+              onClick={() => signOut({ redirect: false })}
+            >
+              <Typography
+                color="black"
+                fontSize="body-16"
+                fontWeight="regular"
+                lineHeight="150%"
+              >
+                로그아웃
+              </Typography>
+            </Button>
+            <Link href={`/account/${session.user?.name}`}>
+              <Typography
+                color="black"
+                fontSize="body-16"
+                fontWeight="regular"
+                lineHeight="150%"
+              >
+                마이페이지
+              </Typography>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link href="/auth/signin">
+              <Typography
+                color="black"
+                fontSize="body-16"
+                fontWeight="regular"
+                lineHeight="150%"
+              >
+                로그인
+              </Typography>
+            </Link>
+            <Link href="/auth/singinup">
+              <Typography
+                color="black"
+                fontSize="body-16"
+                fontWeight="regular"
+                lineHeight="150%"
+              >
+                회원가입
+              </Typography>
+            </Link>
+          </>
+        )}
         <Button variant="Line">문의하기</Button>
       </Styled.Buttons>
     </Styled.Container>
