3a3eb75c783f5001cc62822cee8c02c1874f86cd
박준서 <oponize@naver.com>
2023-01-06 18:44:44 +0900

    feat: error fallback 추가

---
+++ src/components/fallback/error.tsx
@@ -0,0 +1,48 @@
+import Alert from 'components/common/alert';
+import Button from 'components/common/button';
+import Container from 'components/common/container';
+import Typography from 'components/common/typography';
+import * as React from 'react';
+import type { FallbackProps } from 'react-error-boundary';
+
+type ErrorFallbackProps = FallbackProps & {
+  description?: string;
+  margin?: React.CSSProperties['margin'];
+};
+
+const ErrorFallback = ({
+  error,
+  description,
+  margin,
+  resetErrorBoundary,
+}: ErrorFallbackProps) => {
+  return (
+    <Container
+      width="100%"
+      height="100%"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+      gap="20px"
+      margin={margin}
+    >
+      <Alert title={error.message} severity="error" />
+      <Typography
+        fontSize="body-14"
+        fontWeight="regular"
+        color="greyScale-5"
+        lineHeight="150%"
+      >
+        {description
+          ? description
+          : '지속적으로 에러가 발생한다면 문의 게시판에 문의를 남겨주세요.'}
+      </Typography>
+      <Button type="button" variant="Line" onClick={resetErrorBoundary}>
+        재시도
+      </Button>
+    </Container>
+  );
+};
+
+export default ErrorFallback;
