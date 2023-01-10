db89960a2aae2b4e048b7a7dad1afc363d3034c5
박준서 <oponize@naver.com>
2023-01-10 17:16:21 +0900

    feat: Inquiry 페이지 상수 추가

---
+++ src/constants/inquiry.ts
@@ -0,0 +1,33 @@
+interface InquiryLink {
+  title: string;
+  link: string;
+  description?: string;
+}
+
+const inquiry = {
+  links: [
+    {
+      title: '딜러 등록 문의',
+      link: '/inquiry/dealer',
+    },
+    {
+      title: '판매차량 등록 문의',
+      link: '/inquiry/market',
+      description: '판매차량 등록 문의는 딜러 등록을 완료한 후에 가능합니다.',
+    },
+    {
+      title: '딜러 등록 문의',
+      link: '/inquiry/partnership',
+    },
+    {
+      title: '광고 문의',
+      link: '/inquiry/advertisement',
+    },
+    {
+      title: '기타 문의',
+      link: '/inquiry/misc',
+    },
+  ] as InquiryLink[],
+};
+
+export default inquiry;
