ca9c8debcf2f126e8ea70a948a7104270edc0fed
Junseo <oponize@naver.com>
2023-01-10 09:29:57 +0900

    feat: Inquiry 페이지 레이아웃

---
+++ src/pages/inquiry/index.tsx
@@ -0,0 +1,15 @@
+import Container from 'components/common/container';
+import Title from 'components/common/title';
+import Layout from 'components/layout/layout';
+
+const Inquiry = () => {
+  return (
+    <Container>
+      <Title>어떤 문의를 원하시나요?</Title>
+    </Container>
+  );
+};
+
+Inquiry.Layout = Layout;
+
+export default Inquiry;
