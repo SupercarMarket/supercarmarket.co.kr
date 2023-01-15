9091e0ea7b614d44828ca651334d9b6107266844
Junseo <oponize@naver.com>
2023-01-15 12:32:24 +0900

    feat: 차량판매 문의 페이지 마크업 구현

--- src/pages/inquiry/market.tsx
+++ src/pages/inquiry/market.tsx
@@ -1,10 +1,14 @@
+import Container from 'components/common/container';
+import Title from 'components/common/title';
+import { SaleForm } from 'components/inquiry';
 import Layout from 'components/layout/layout';
 
 const Market = () => {
   return (
-    <div>
-      <h1>매물</h1>
-    </div>
+    <Container display="flex" flexDirection="column" gap="40px">
+      <Title>판매차량 등록 문의</Title>
+      <SaleForm />
+    </Container>
   );
 };
 
