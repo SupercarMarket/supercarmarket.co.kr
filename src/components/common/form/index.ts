cd2d1a5528f227626e21b5063767bb6b50b69dff
박준서 <oponize@naver.com>
2023-01-27 14:21:58 +0900

    feat: formCheckbox Component 구현

--- src/components/common/form/index.ts
+++ src/components/common/form/index.ts
@@ -1,6 +1,10 @@
 export { default as Form } from './form';
+export { default as FormCheckbox } from './formCheckbox';
+export { default as FormFiles } from './formFiles';
+export { default as FormImages } from './formImages';
 export { default as FormInput } from './formInput';
 export { default as FormLabel } from './formLabel';
 export { default as FormMessage } from './formMessage';
 export { default as FormPostcode } from './formPostcode';
+export { default as FormRange } from './formRange';
 export { default as FormSelect } from './formSelect';
