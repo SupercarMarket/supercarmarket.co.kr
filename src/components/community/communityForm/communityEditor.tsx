046e4c54177eff778e26409f228061b480f9ddb5
박준서 <oponize@naver.com>
2023-02-06 20:31:38 +0900

    feat: tui instance로 초기값 지정

--- src/components/community/communityForm/communityEditor.tsx
+++ src/components/community/communityForm/communityEditor.tsx
@@ -4,7 +4,20 @@ import { Editor } from '@toast-ui/react-editor';
 import * as React from 'react';
 
 const CommunityEditor = () => {
-  return <Editor initialValue="" initialEditType="markdown" />;
+  const editor = React.useRef<InstanceType<typeof Editor>>(null);
+
+  React.useEffect(() => {
+    const instance = editor.current?.getInstance();
+
+    instance?.setHTML('');
+  }, []);
+  return (
+    <Editor
+      ref={editor}
+      placeholder="내용을 입력해주세요."
+      usageStatistics={false}
+    />
+  );
 };
 
 export default CommunityEditor;
