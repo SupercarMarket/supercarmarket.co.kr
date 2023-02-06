d9d850eec6b4a261fb7c04f836df51935f5304d1
Junseo <oponize@naver.com>
2023-02-06 13:33:33 +0900

    feat: Community Page 초기화

--- src/pages/community/[subject]/[category]/index.tsx
+++ src/pages/community/[subject]/[category]/index.tsx
@@ -1,12 +1,33 @@
 import Container from 'components/common/container';
+import Tab from 'components/common/tab';
 import Title from 'components/common/title';
+import Wrapper from 'components/common/wrapper';
+import CommunityNavbar from 'components/community/communityNavbar';
 import layout from 'components/layout';
+import { css } from 'styled-components';
 
 const CommunityCategory = () => {
   return (
     <Container>
       <Title>커뮤니티 인기글</Title>
-      <Title>제보</Title>
+      <Wrapper
+        css={css`
+          position: relative;
+        `}
+      >
+        <Wrapper.Item
+          css={css`
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+          `}
+        >
+          <Title>제보</Title>
+          <Tab />
+        </Wrapper.Item>
+        <CommunityNavbar />
+      </Wrapper>
     </Container>
   );
 };
---
+++ src/pages/community/create.tsx
@@ -0,0 +1,14 @@
+import Container from 'components/common/container';
+import layout from 'components/layout';
+
+const Create = () => {
+  return (
+    <Container>
+      <h1>작성</h1>
+    </Container>
+  );
+};
+
+Create.Layout = layout;
+
+export default Create;
