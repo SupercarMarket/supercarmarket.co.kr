f88af2f7c8a18e1680b58d7f0acbe4bb70325d3f
Junseo <oponize@naver.com>
2023-03-04 16:38:24 +0900

    feat: lib package util 추가

---
+++ packages/lib/text.ts
@@ -0,0 +1,22 @@
+export const truncate = (text: string, maxLength: number, ellipsis = true) => {
+  if (text.length <= maxLength) return text;
+  return `${text.slice(0, maxLength - 3)}${ellipsis ? '...' : ''}`;
+};
+
+export const truncateOnWord = (
+  text: string,
+  maxLength: number,
+  ellipsis = true
+) => {
+  if (text.length <= maxLength) return text;
+
+  let truncatedText = text.substring(0, 148);
+
+  truncatedText = truncatedText.substring(
+    0,
+    Math.min(truncatedText.length, truncatedText.lastIndexOf(' '))
+  );
+  if (ellipsis) truncatedText += '...';
+
+  return truncatedText;
+};
