58a79e69eb110475970746626812a290d9c5cf17
seongjin <seongjin2427@gmail.com>
2023-03-31 18:09:41 +0900

    feat(web): 삭제 및 판매완료 로직 추가

--- apps/web/src/components/market/marketDetail/components/marketMine/marketMine.tsx
+++ apps/web/src/components/market/marketDetail/components/marketMine/marketMine.tsx
@@ -3,12 +3,14 @@ import {
   Typography,
   Wrapper,
   applyMediaQuery,
+  theme,
 } from '@supercarmarket/ui';
+import { Modal } from 'components/common/modal';
+import ModalContext from 'feature/modalContext';
 import {
   useDeleteMarketPost,
   useUpdateMarketSellStatus,
 } from 'http/server/market';
-import { useSession } from 'next-auth/react';
 import * as React from 'react';
 import { css } from 'styled-components';
 
@@ -18,24 +20,59 @@ interface MarketMineProps {
 }
 
 const MarketMine = ({ id, brdSeq }: MarketMineProps) => {
-  const session = useSession();
-
   const { mutate: removeMarketById } = useDeleteMarketPost();
   const { mutate: changeSellStatus } = useUpdateMarketSellStatus();
+  const { onOpen, onClose } = React.useContext(ModalContext);
 
-  const removeMarket = () => {
-    if (!session.data) return;
-    removeMarketById({
-      data: [{ id }],
-    });
-  };
-
-  const changeStatus = () => {
-    if (!session.data) return;
+  const changeStatus = React.useCallback(() => {
     changeSellStatus({
       data: { brdSeq },
     });
-  };
+  }, [brdSeq, changeSellStatus]);
+
+  const removeMarket = React.useCallback(() => {
+    removeMarketById({
+      data: [{ id }],
+    });
+  }, [id, removeMarketById]);
+
+  const handleStatusModal = React.useCallback(() => {
+    onOpen(
+      <Modal
+        title="매물 판매 완료"
+        description={`매물을 판매완료 처리 하시겠습니까?\n판매 완료 후, 상태를 변경하실 수 없습니다.`}
+        background={theme.color['greyScale-6']}
+        closeText="취소"
+        clickText="판매완료"
+        onClose={() => {
+          onClose();
+        }}
+        onCancel={() => {
+          onClose();
+        }}
+        onClick={changeStatus}
+      />
+    );
+  }, [changeStatus, onOpen, onClose]);
+
+  const handleRemoveModal = React.useCallback(() => {
+    onOpen(
+      <Modal
+        title="매물 삭제"
+        description={`매물을 삭제하시겠습니까?\n삭제 후 글을 복구할 수 없습니다.`}
+        background={theme.color['greyScale-6']}
+        closeText="취소"
+        clickText="판매완료"
+        onClose={() => {
+          onClose();
+        }}
+        onCancel={() => {
+          onClose();
+        }}
+        onClick={removeMarket}
+      />
+    );
+  }, [removeMarket, onOpen, onClose]);
 
   return (
     <Wrapper
@@ -50,10 +87,10 @@ const MarketMine = ({ id, brdSeq }: MarketMineProps) => {
         }
       `}
     >
-      <Button variant="Line" onClick={removeMarket}>
+      <Button variant="Line" onClick={handleRemoveModal}>
         <Typography color="greyScale-6">삭제</Typography>
       </Button>
-      <Button variant="Line" onClick={changeStatus}>
+      <Button variant="Line" onClick={handleStatusModal}>
         <Typography color="greyScale-6">판매 완료</Typography>
       </Button>
     </Wrapper>
