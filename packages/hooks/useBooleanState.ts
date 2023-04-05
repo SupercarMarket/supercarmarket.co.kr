205bab55f02ee245d8194cfd413dc95e80a41a13
박준서 <oponize@naver.com>
2023-04-05 22:31:31 +0900

    feat(hooks): useBooleanState 훅 추가

---
+++ packages/hooks/useBooleanState.ts
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+export const useBooleanState = (
+  defaultValue = false
+): readonly [boolean, VoidFunction, VoidFunction, VoidFunction] => {
+  const [bool, setBool] = React.useState(defaultValue);
+
+  const setTrue = React.useCallback(() => {
+    setBool(true);
+  }, []);
+
+  const setFalse = React.useCallback(() => {
+    setBool(false);
+  }, []);
+
+  const toggle = React.useCallback(() => {
+    setBool((b) => !b);
+  }, []);
+
+  return [bool, setTrue, setFalse, toggle] as const;
+};
