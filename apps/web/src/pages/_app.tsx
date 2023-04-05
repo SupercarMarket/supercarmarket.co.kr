7b7d40cec5e704dfeccb03a8373c0ded69988e56
박준서 <oponize@naver.com>
2023-04-05 19:35:49 +0900

    chore(web): 뷰포트 최대 스케일 2

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -89,7 +89,7 @@ function MyApp({
               <Head>
                 <meta
                   name="viewport"
-                  content="width=device-width,initial-scale=1.0,maximum-scale=1.0"
+                  content="width=device-width,initial-scale=1.0,maximum-scale=2.0"
                 />
               </Head>
               <DefaultSeo
