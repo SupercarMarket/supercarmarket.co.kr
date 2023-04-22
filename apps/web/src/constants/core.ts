a78e1a0b438fb38867d40790a31c09905b1b7c76
박준서 <oponize@naver.com>
2023-04-22 12:25:38 +0900

    fix(web): SEO title 변경

--- apps/web/src/constants/core.ts
+++ apps/web/src/constants/core.ts
@@ -2,9 +2,9 @@ export const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_URL
   ? `https://${process.env.NEXT_PUBLIC_URL}`
   : '';
 
-export const PRODUCT_URL = 'https://supercar-market.vercel.app';
+export const PRODUCT_URL = 'https://doyup.shop';
 
-export const APP_NAME = 'Supercarmarket.com';
+export const APP_NAME = 'Supercarmarket.co.kr';
 export const LOGO = '/images/logo/favicon.ico';
 export const LOGO_ICON = '/images/logo/favicon.ico';
 export const SEO_IMG_DEFAULT = `${NEXT_PUBLIC_BASE_URL}/og.png`;
