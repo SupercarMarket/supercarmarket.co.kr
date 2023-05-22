cb11623c96f19ba0ced94ffcc3e255854807c881
doyupKim <gltlvl12@gmail.com>
2023-05-22 23:30:23 +0900

    change mobile camera device test#7

--- apps/web/src/components/live/Publisher.tsx
+++ apps/web/src/components/live/Publisher.tsx
@@ -164,8 +164,8 @@ function Publisher(props: Props) {
             resolution: '880x495',
             frameRate: 60,
             videoSource: isMobile
-              ? videoDevices[0].deviceId
-              : videoDevices[1].deviceId,
+              ? videoDevices[1].deviceId
+              : videoDevices[0].deviceId,
             audioSource: undefined,
           });
 
