6aee8546a552b636a17920e55df49551e7699b4d
doyupK <gltlvl12@gmail.com>
2023-09-27 19:38:18 +0900

    fix. editor css

--- apps/web/src/styles/globalStyles.ts
+++ apps/web/src/styles/globalStyles.ts
@@ -32,7 +32,12 @@ const GlobalStyle = createGlobalStyle`
     cursor: pointer;
   }
 
-  .toastui-editor-contents * {
+  .toastui-editor-contents div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .toastui-editor-contents {
     display: flex;
     flex-direction: column;
     align-items: center;
