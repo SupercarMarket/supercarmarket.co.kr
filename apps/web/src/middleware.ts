f5457bb71f7f6653fe57fb3108403e9e6d035fd7
doyupK <gltlvl12@gmail.com>
2024-07-03 22:37:27 +0900

    fix lint

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -20,17 +20,16 @@ const rateLimitStore: Record<string, { count: number; timestamp: number }> = {};
 async function rateLimiter(req: NextRequest) {
   let ip = req.headers.get('x-forwarded-for');
   if (!ip) {
-    ip = req.headers.get('cf-connecting-ip') || 
-    req.headers.get('x-real-ip') || 
-    req.headers.get('fastly-client-ip') || 
-    req.headers.get('True-Client-IP') || 
-    req.headers.get('x-client-ip') || 
-    req.headers.get('x-cluster-client-ip') || 
-    req.ip || 
+    ip = req.headers.get('cf-connecting-ip') ||
+    req.headers.get('x-real-ip') ||
+    req.headers.get('fastly-client-ip') ||
+    req.headers.get('True-Client-IP') ||
+    req.headers.get('x-client-ip') ||
+    req.headers.get('x-cluster-client-ip') ||
+    req.ip ||
     'unknown';
   }
 
-  
   if (blockedIPs[ip]) {
     const currentTime = Date.now();
     if (currentTime - blockedIPs[ip] > 24 * 60 * 60 * 1000) {
@@ -38,10 +37,10 @@ async function rateLimiter(req: NextRequest) {
     } else {
       return NextResponse.json(
         {
-          error: 'Your IP has been temporarily blocked due to excessive requests.' 
-        }, 
+          error: 'Your IP has been temporarily blocked due to excessive requests.'
+        },
         { 
-          status: 403 
+          status: 403
         }
       );
     }
@@ -61,14 +60,13 @@ async function rateLimiter(req: NextRequest) {
     blockedIPs[ip] = currentTime;
     return NextResponse.json(
       { 
-        error: 'Too many requests, please try again later.' 
+        error: 'Too many requests, please try again later.'
       },
       {
         status: 429
       }
     );
   }
-
   return null;
 }
 
